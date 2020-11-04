308be33afaeb70129f2a43e780c2d9c9c3abc2a2
Sergey Vlasovv <35797418+SergeyVlasov@users.noreply.github.com>
2020-11-04 16:18:10 +0300

    Update server.go

--- server/server.go
+++ server/server.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	_ "github.com/lib/pq"
 	"github.com/gorilla/mux"
-
+    //"time"
 	"strings"
 	
     "os"
@@ -23,7 +23,7 @@ var database *sql.DB
 
 
 func main() {
-	// конфиг подключения к БД
+	// read config
 	file, err := os.Open("dbconfig.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -34,7 +34,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+    //fmt.Println(string(data[:config]))
 
 	//db, err := sql.Open("postgres", "dbname=worktime user=postgres password=poilo777 host=localhost sslmode=disable")
 	db, err := sql.Open("postgres", string(data[:config]))
@@ -52,6 +52,8 @@ func main() {
 
     router := mux.NewRouter()
 	router.HandleFunc("/", MainPageHandler)
+	router.HandleFunc("/{iduser}", DatePageHandler)
+	router.HandleFunc("/{iduser}/{date}", WorktimePage)
 	router.HandleFunc("/{date}/{time}/{iduser}/{inout}", AddCheck)
     http.Handle("/",router)
     fmt.Println("Server is listening...")
@@ -61,17 +63,18 @@ func main() {
 }
 
 
-type User struct {
-	Number int
-	Name1  string
-	Name2  string
-	Name3  string
-}
 
-func MainPageHandler(w http.ResponseWriter, r *http.Request) { // находимся на главной странице  www.XXX.xx/
+func MainPageHandler(w http.ResponseWriter, r *http.Request) { // находимся на главной странице  
 
-	rows, err := database.Query("select  number, name1, name2, name3 from public.users;")
+	type User struct {
+		Id int
+		Number int
+		Name1  string
+		Name2  string
+		Name3  string
+	}
 
+	rows, err := database.Query("select  id, number, name1, name2, name3 from public.users;")
 	if err != nil {
 		log.Println(err)
 	}
@@ -79,22 +82,58 @@ func MainPageHandler(w http.ResponseWriter, r *http.Request) { // находим
 	user_current := []User{}
 	for rows.Next() {
 		p := User{}
-		err := rows.Scan(&p.Number, &p.Name1, &p.Name2, &p.Name3 )
+		err := rows.Scan(&p.Id, &p.Number, &p.Name1, &p.Name2, &p.Name3 )
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 		user_current = append(user_current, p)
 	}
-
 	tmpl, _ := template.ParseFiles("templates/main_page.html")
 	w.Header().Set("Content-Type", "text/html")
 	tmpl.Execute(w, user_current)
-
 }
 
 
 
+func DatePageHandler(w http.ResponseWriter, r *http.Request) { // находимся на странице  выбора даты
+	tmpl, _ := template.ParseFiles("templates/page_data.html")
+	//w.Header().Set("Content-Type", "text/html")
+	tmpl.Execute(w, nil)
+}
+
+
+
+func WorktimePage(w http.ResponseWriter, r *http.Request) { // страница записей
+	vars := mux.Vars(r)
+	iduser := str_handle(vars["iduser"])
+	date := str_handle(vars["date"])
+
+	type Work struct {
+	    Time  string
+		Inout  int
+	}
+
+	rows, err := database.Query("select  time, inout from public.checktime WHERE iduser = "+iduser+" AND date = '"+date+"';")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	work_current := []Work{}
+	for rows.Next() {
+		p := Work{}
+		err := rows.Scan(&p.Time, &p.Inout)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		work_current = append(work_current, p)
+	}
+	tmpl, _ := template.ParseFiles("templates/work.html")
+	//w.Header().Set("Content-Type", "text/html")
+	tmpl.Execute(w, work_current)
+
+}
 
 
 // добавляем запись в таблицу учета времени
@@ -104,6 +143,7 @@ func AddCheck(w http.ResponseWriter, r *http.Request) {
 	time := str_handle(vars["time"])
 	iduser := str_handle(vars["iduser"])
 	inout := str_handle(vars["inout"])
+
 
 	// проверка когда была сделана последняя запись (чтобы не дублировать близкие по времени)
 	type Time struct {
@@ -142,10 +182,12 @@ func AddCheck(w http.ResponseWriter, r *http.Request) {
 			b2, _ := strconv.Atoi(time_compare[1])
 			b3, _ := strconv.Atoi(time_compare[2])
 	
-			time_different := (a1*3600 + a2*60 + a3) - (b1*3600 + b2*60 + b3)
 	
-			//если прошлая запись была сделана не менее 10 секунд назад то вносим новую запись
-			if (time_different > 10) {
+			time_different := (a1*3600 + a2*60 + a3) - (b1*3600 + b2*60 + b3)  // подсчет сколько времени прошло с последней записи
+	
+			//fmt.Println(time_different)
+	
+			if (time_different > 5) {
 				// вносим запись если последняя была сделана не недавно
 				rows, err := database.Query("INSERT INTO public.checktime(date, time, iduser, inout) VALUES ('" + date + "', '" + time + "', " + iduser + "," + inout + ");")
 	            if err != nil {
@@ -155,12 +197,7 @@ func AddCheck(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
-	
-	
-
 }
-
-
 
 
 
